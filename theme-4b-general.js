b22b7607ab274642297e2e51e3f41db2bb151f35
Nicola Hastings <nicolahastings@gmail.com>
2020-06-02 15:56:31 -0400

    NH | Adding icons for ACL

--- theme-4b-general.js
+++ theme-4b-general.js
@@ -5,11 +5,39 @@ $(function () {
 
 function handleAllContentList() {
     $('.HLLandingControl.SearchResults ul li').each(function () {
+
+        // handle byline and posted in
+
         var self = $(this),
             byline = $(self).find('.ByLine'),
             postedIn = $(self).find('h5');
 
         $(postedIn).insertAfter(byline);
+
+        // handle icons
+
+        var label = $(self).find('.title-row > .col-md-3 > .label-default'),
+            labelText = $(label).text();
+
+        labelText = $.trim(labelText);
+
+        switch (labelText) {
+            case 'Announcement':
+                $(label).addClass('announcement');
+                break;
+            case 'Discussion':
+                $(label).addClass('discussion');
+                break;
+            case 'Blog Entry':
+                $(label).addClass('blog');
+                break;
+            case 'Event':
+                $(label).addClass('event');
+                break;
+            default:
+                $(label).addClass('default');
+                break;
+        }
     });
 }
 
